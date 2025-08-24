27749309b32269a0d2ae8bc9a2c21540f7eb6731
danielgamble <dgamble94115@uat.edu>
2025-08-23 20:10:24 -0400

    Add files via upload

--- Input Example.js
+++ Input Example.js
@@ -1,13 +1,13 @@
  // function to validate our form inputs
         function validateForm(){
             // get the user's name so we can validate it
-            let fullName = document.getElementById("txtFullName").value;
+            let fullName=document.getElementById("txtFullName").value;
 
             // get the user's favorite color so we can validate it
-            let favColor = document.getElementById("txtFavColor").value;
+            let favColor=document.getElementById("txtFavColor").value;
 
             // shortcut to the message div
-            let divMessage =document.getElementById("divMessage");
+            let divMessage=document.getElementById("divMessage").value;
 
             // Pulls up the input into the console
             console.log("fullName=" + fullName);
@@ -17,9 +17,13 @@
             // equality uses double equal sign - = - checks to see if value on the left hand side equals value on right hand
             // === is strict equality - the values and the data type have to match
             // in our if / else if statements we always are checking equality
-            if(fullName.trim() = ""){
+            
+
+            if (fullName.trim() === "") {
                 divMessage.textContent = "Name cannot be blank";
-            }
+             }
+
+           
 
             // prevent the form from submitting 
             return false;
